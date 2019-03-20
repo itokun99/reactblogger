cb46fbeab7ebf59055544d16e5d0bf471fe19e52
indrawan <indrawanlisantopersonal@gmail.com>
2019-03-21 02:54:52 +0700

    Add test buid

---
+++ test_build/precache-manifest.2bce1c578efcfdc5e5d871a909bead7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c3823964930a5fc234ab",
+    "url": "/static/js/main.a8d5b71b.chunk.js"
+  },
+  {
+    "revision": "e21d7407e18f6262dd0a",
+    "url": "/static/js/2.4d93de05.chunk.js"
+  },
+  {
+    "revision": "c3823964930a5fc234ab",
+    "url": "/static/css/main.3ff746a8.chunk.css"
+  },
+  {
+    "revision": "285a25143747ed08a0b319ee4c73871a",
+    "url": "/index.html"
+  }
+];
