972f92d544e42665069c0f3c382a91061d10e404
indrawan <indrawanlisantopersonal@gmail.com>
2019-03-23 21:31:35 +0700

    Prod Build

---
+++ prod/build/precache-manifest.a127dd505c99b054d682396a1a8edafb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "958201b36685016120b4",
+    "url": "/static/js/main.59c02840.chunk.js"
+  },
+  {
+    "revision": "fa84055c9a7e0c818229",
+    "url": "/static/js/2.8bd29d98.chunk.js"
+  },
+  {
+    "revision": "958201b36685016120b4",
+    "url": "/static/css/main.7553f1a5.chunk.css"
+  },
+  {
+    "revision": "8844ceb12659925236a3e05ef07c5489",
+    "url": "/index.html"
+  }
+];
