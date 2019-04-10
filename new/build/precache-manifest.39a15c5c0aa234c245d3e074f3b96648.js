094940652671080fc630b93cc0335eadc0471f00
Indrawan Lisanto <45938169+itokun99@users.noreply.github.com>
2019-04-11 01:50:05 +0700

    Add files via upload

---
+++ new/build/precache-manifest.39a15c5c0aa234c245d3e074f3b96648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "1df6e8180dcb4a37efb7",
+    "url": "/static/js/main.7595c959.chunk.js"
+  },
+  {
+    "revision": "13ac76ba67dfab99f46b",
+    "url": "/static/js/2.acefbed5.chunk.js"
+  },
+  {
+    "revision": "1df6e8180dcb4a37efb7",
+    "url": "/static/css/main.8170c419.chunk.css"
+  },
+  {
+    "revision": "13ac76ba67dfab99f46b",
+    "url": "/static/css/2.bd038489.chunk.css"
+  },
+  {
+    "revision": "63396bf05f0b5c80bdb53f8c0aa4ded0",
+    "url": "/index.html"
+  }
+];
